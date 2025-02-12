25a65efe210109798aaa60e0c75305cceb223112
Elsie <elsie19@pm.me>
2025-02-12 16:57:34 -0500

    Add tail command

--- app/tail.f90
+++ app/tail.f90
@@ -1,13 +1,17 @@
 program tail
+    use funix_utils, only: EX_BAD
     use flap, only: command_line_interface
+    use iso_fortran_env, only: error_unit
 
     implicit none
 
     type(command_line_interface) :: cli
-    integer :: lines
-    integer :: count
+    integer :: unit, ios, lines, line_count = 0, n_lines_to_read = 0, i, start_index
+    logical :: file_exists
     integer :: error
-    character(len=90) :: file
+    character(len=100) :: file
+    character(len=255) :: line
+    character(len=255), allocatable :: lines_array(:)
 
     call cli%init(progname='tail',      &
         version='0.0.1',                &
@@ -25,11 +29,43 @@ program tail
     if (error /= 0) stop
     call cli%get(switch='-n', val=lines, error=error) ; if (error /= 0) stop
     call cli%get(position=1, val=file, error=error) ; if (error /= 0) stop
-    print '(a)', cli%progname // ' has been called with the following argument:'
-    call cli%free()
-    print '(a, i0)', "Lines = ", lines
-    print '(a, a)', "File = ", file
 
-    count = command_argument_count()
-    print '(i0)', count
+    inquire(file=file, exist=file_exists)
+    if (.not. file_exists) then
+        write (error_unit, '(a)'), cli%progname // ": cannot open " // "'" // trim(file) // "'" // " for reading: No such file or directory"
+        call exit(EX_BAD)
+    end if
+
+    call cli%free()
+
+    open(unit=unit, file=file, status='old')
+
+    line_count = 0
+    do while (.true.)
+        read(unit, '(A)', iostat=ios) line
+        if (ios /= 0) exit
+        line_count = line_count + 1
+    end do
+
+    allocate(lines_array(line_count))
+
+    rewind(unit)
+    line_count = 0
+    do while (.true.)
+        read(unit, '(A)', iostat=ios) line
+        if (ios /= 0) exit
+        line_count = line_count + 1
+        lines_array(line_count) = trim(adjustl(line))
+    end do
+
+    close(unit)
+
+    n_lines_to_read = 0
+    start_index = max(1, line_count - lines + 1)
+    do i = start_index, line_count
+        print '(A)', trim(adjustl(lines_array(i)))
+        n_lines_to_read = n_lines_to_read + 1
+        if (n_lines_to_read == lines) exit
+    end do
+
 end program tail
--- src/funix-utils.f90
+++ src/funix-utils.f90
@@ -1,10 +1,20 @@
+!> Module containing many useful components relating to builtin utils.
 module funix_utils
-  implicit none
-  private
+    implicit none
+    private
 
-  public :: say_hello
+    public :: say_hello
+
+    public EX_OK, EX_BAD, EX_MISUSE
+
+    !> Ok exit code.
+    integer, parameter :: EX_OK = 0
+    !> Bad exit code.
+    integer, parameter :: EX_BAD = 1
+    !> Builtin misuse exit code.
+    integer, parameter :: EX_MISUSE = 2
 contains
-  subroutine say_hello
-    print *, "Hello, funix-utils!"
-  end subroutine say_hello
+    subroutine say_hello
+        print *, "Hello, funix-utils!"
+    end subroutine say_hello
 end module funix_utils
